7ce79736c3d1adc8e73b91f9831175b490b9a155
NageswararaoNilla <nillanageswararao075@gmail.com>
2023-08-09 05:28:03 +0000

    Debugging completed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,8 +6,7 @@ import './index.css'
 const Header = props => {
   const onClickLogout = () => {
     const {history} = props
-
-    Cookies.remove('jwt')
+    Cookies.remove('jwt_token')
     history.replace('/login')
   }
 
@@ -104,4 +103,4 @@ const Header = props => {
   )
 }
 
-export default Header
+export default withRouter(Header)
